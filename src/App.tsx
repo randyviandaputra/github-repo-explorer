e5731ab9ecc9c1f83dd8d9ba359ebc5c5a89dcd1
Randy Vianda Putra <modulloe@gmail.com>
2025-03-10 02:35:51 +0700

    deps(react-query): fix broken version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 
 import SearchForm from "./components/SearchForm";
